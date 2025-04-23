454d38884a0d8cc1cf6303484690d8057df2af97
Zoheb Sharif <46800902+ZohebSharif@users.noreply.github.com>
2025-04-22 19:52:14 -0700

    added chat implementation

--- Phase 3 Documents/ChatRelayProject/src/chatRelay/Chat.java
+++ Phase 3 Documents/ChatRelayProject/src/chatRelay/Chat.java
@@ -9,11 +9,83 @@ public class Chat {
     private User[] chatters;
     private Boolean isPrivate = false;
 
-    public Chat(User chatOwner, String name) {}
-
-    public void addChatter() {}
-    public void removeChatter() {}
-    public void addMessage(Message msg) {}
-    public void changePrivacy(Boolean newState) {}
-    public String toString() { return ""; }
+// constructor takes in a user object and string for user
+    public Chat(User chatOwner, String name) {
+        this.owner = chatOwner;
+        this.roomName = name;
+        this.id = "CHAT_" + (++count);
+        this.messages = new Message[0];
+        this.chatters = new User[1];
+        this.chatters[0] = chatOwner;
+    }
+    // add a chatter
+    public void addChatter(User user) {
+        User[] newChatters = new User[chatters.length + 1];
+        for (int i = 0; i < chatters.length; i++) {
+            newChatters[i] = chatters[i];
+        }
+        newChatters[chatters.length] = user;
+        chatters = newChatters;
+    }
+    // remove a chatter
+    public void removeChatter(User user) {
+        if (chatters.length <= 1) {
+            return; // cannot remove only user
+        }
+        
+        User[] newChatters = new User[chatters.length - 1];
+        int index = 0;
+        for (int i = 0; i < chatters.length; i++) {
+            if (!chatters[i].getId().equals(user.getId())) {
+                newChatters[index++] = chatters[i];
+            }
+        }
+        
+        // only update if user was found and removed
+        if (index == newChatters.length) {
+            chatters = newChatters;
+        }
+    }
+    // add a message
+    public void addMessage(Message msg) {
+        Message[] newMessages = new Message[messages.length + 1];
+        for (int i = 0; i < messages.length; i++) {
+            newMessages[i] = messages[i];
+        }
+        newMessages[messages.length] = msg;
+        messages = newMessages;
+    }
+    // change privacy to provided state
+    public void changePrivacy(Boolean newState) {
+        this.isPrivate = newState;
+    }
+    // return string representation of chat
+    public String toString() {
+        return "Chat [id=" + id + ", roomName=" + roomName + ", owner=" + owner.getUserName() + ", private=" + isPrivate + ", chatters=" + chatters.length + "]";
+    }
+    
+    // getters
+    public String getId() {
+        return id;
+    }
+    
+    public String getRoomName() {
+        return roomName;
+    }
+    
+    public User getOwner() {
+        return owner;
+    }
+    
+    public Message[] getMessages() {
+        return messages;
+    }
+    
+    public User[] getChatters() {
+        return chatters;
+    }
+    
+    public Boolean isPrivate() {
+        return isPrivate;
+    }
 }
