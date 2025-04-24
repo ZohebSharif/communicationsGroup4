aec0e1b34e5e6cb20bf994ffbd43d54ecd04a058
Kenny <kennykotten@gmail.com>
2025-04-23 23:16:07 -0700

    add database folders/text files to hookup to DBManager. Put a main() in Server.java.

--- Phase 3 Documents/ChatRelayProject/src/chatRelay/DBManager.java
+++ Phase 3 Documents/ChatRelayProject/src/chatRelay/DBManager.java
@@ -1,38 +1,83 @@
 package chatRelay;
 
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class DBManager {
-    
-    //private HashMap<userId, User> users;
-    //private HashMap<chatId, Chat> chats;
-    //private HashMap<messageId, Message> messages;
-    private Server server;
-    private String txtFilePath;
-    private String userTxtFilename;
-    private String chatTxtFilename;
-    private String messageTxtFilename;
 
-    public DBManager(String filepath, String userTxtFilename, String chatTxtFilename, String messageTxtFilename) {}
+	// private HashMap<userId, User> users;
+	// private HashMap<chatId, Chat> chats;
+	// private HashMap<messageId, Message> messages;
+	private Server server;
+	private String txtFilePath;
+	private String userTxtFilename;
+	private String chatTxtFilename;
+	private String messageTxtFilename;
 
-    public void loadAllFiles() {}
-    //public User checkLoginCredentials(String username, String password) {}
-    public void sendUserAllFreshData() {}
-    //public User getUserById(String userId) {}
-    //public Chat getChatById(String chatId) {}
-    //public Message getMessageById(String messageId) {}
-    //public List<User> fetchAllUsers() {}
-    //public List<Chat> fetchAllChats() {}
-    //public List<Message> fetchAllMessages() {}
-    //public List<Chat> getChatsForUser(String userId) {}
-    public void writeNewUser(User user) {}
-    public void writeNewChat(Chat chat) {}
-    public void writeNewMessage(Message message) {}
-    //private User stringToUser(String userString) {}
-    //private Chat stringToChat(String chatString) {}
-    //private Message StringToMessage(String messageString) {}
-    private void getSanitizedCharacter(String input, String output) {}
-    public void addUserToChat(String userId) {}
-    //public Boolean usernameExists(String name) {}
-    //private Boolean validUsername(String name) {}
+//    filepath = "./src/chatRelay/dbFiles/development/"    (navigate from root path!)
+	public DBManager(String filepath, String userTxtFilename, String chatTxtFilename, String messageTxtFilename) {
+		this.txtFilePath = filepath;
+		this.userTxtFilename = userTxtFilename; // username/password/id/firstName/lastName/isDisabled/isAdmin
+		this.chatTxtFilename = chatTxtFilename; // id/owner/roomName/[userId1, userId2, userId3]/isPrivate
+		this.messageTxtFilename = messageTxtFilename; // id/createdAt/content/authorId/chatId
+
+//TESTING
+
+		print(filepath + userTxtFilename);
+		print(filepath + chatTxtFilename);
+		print(filepath + messageTxtFilename);
+	}
+
+// FORMATS:	
+
+// TESTER
+	private void print(String fullPath) {
+		try (Scanner scanner = new Scanner(new File(fullPath))) {
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				System.out.println(line);
+			}
+			System.out.println("+++++++++++++++++++");
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+	}
+
+	public void loadAllFiles() {
+	}
+
+	// public User checkLoginCredentials(String username, String password) {}
+	public void sendUserAllFreshData() {
+	}
+
+	// public User getUserById(String userId) {}
+	// public Chat getChatById(String chatId) {}
+	// public Message getMessageById(String messageId) {}
+	// public List<User> fetchAllUsers() {}
+	// public List<Chat> fetchAllChats() {}
+	// public List<Message> fetchAllMessages() {}
+	// public List<Chat> getChatsForUser(String userId) {}
+	public void writeNewUser(User user) {
+	}
+
+	public void writeNewChat(Chat chat) {
+	}
+
+	public void writeNewMessage(Message message) {
+	}
+
+	// private User stringToUser(String userString) {}
+	// private Chat stringToChat(String chatString) {}
+	// private Message StringToMessage(String messageString) {}
+	private void getSanitizedCharacter(String input, String output) {
+	}
+
+	public void addUserToChat(String userId) {
+	}
+	// public Boolean usernameExists(String name) {}
+	// private Boolean validUsername(String name) {}
 }
--- Phase 3 Documents/ChatRelayProject/src/chatRelay/Server.java
+++ Phase 3 Documents/ChatRelayProject/src/chatRelay/Server.java
@@ -7,40 +7,61 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Server {
-    private HashMap<String, ClientHandler> clients;
-    //private ArrayList<ClientHandler> clients; // Testing purposes
-    private DBManager dbManager;
-    private int port;
-    private String IP;
+	private HashMap<String, ClientHandler> clients;
+	// private ArrayList<ClientHandler> clients; // Testing purposes
+	private DBManager dbManager;
+	private int port;
+	private String IP;
 
-    public Server(int port, String IP) {
-        this.port = port;
-        this.IP = IP;
-    }
+	public Server(int port, String IP) {
+		this.port = port;
+		this.IP = IP;
 
-    public void connect() {
-        ServerSocket server;
+		this.dbManager = new DBManager("./src/chatRelay/dbFiles/development/", "Users.txt", "Chats.txt", "Messages.txt");
 
-        try {
-            server = new ServerSocket(port);
-            server.setReuseAddress(true);
+	}
 
-            while(true) {
-                Socket client = server.accept();
+//	public void connect() {
+//		ServerSocket server;
+//
+//		try {
+//			server = new ServerSocket(port);
+//			server.setReuseAddress(true);
+//
+//			while (true) {
+//				Socket client = server.accept();
+//
+//				ClientHandler clientSock = new ClientHandler(client, this);
+//
+//				// clients.add(clientSock);
+//
+//				new Thread(clientSock).start();
+//			}
+//		} catch (IOException e) {
+//
+//		}
+//	}
 
-                ClientHandler clientSock = new ClientHandler(client, this);
+	public void disconnect() {
+	}
 
-                //clients.add(clientSock);
+	public void recievePacket(String clientId, Packet packet) {
+	}
 
-                new Thread(clientSock).start();
-            }
-        } catch (IOException e) {
-            
-        }
-    }
-    public void disconnect() {}
-    public void recievePacket(String clientId, Packet packet) {}
-    public void sendErrorMessage(String userId, String errorMessage) {}
-    public void sendSuccessMessage(String userId, String successMessage) {}
-    public void sendPacketToUsers(Packet packet, String[] userIds) {}
+	public void sendErrorMessage(String userId, String errorMessage) {
+	}
+
+	public void sendSuccessMessage(String userId, String successMessage) {
+	}
+
+	public void sendPacketToUsers(Packet packet, String[] userIds) {
+	}
+
+	public static void main(String[] args) {
+		int port = 1337;
+		String IP = "127.0.0.1";
+
+		System.out.println("Server.java's main() fired");
+		Server server = new Server(port, IP);
+	}
 }
