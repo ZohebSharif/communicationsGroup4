efbd691250c722e477892c19d23e384bbdab163b
Kenny <kennykotten@gmail.com>
2025-04-24 01:33:48 -0700

    put userIds at end of Chat.txt line, make progress on loadChats()

--- Phase 3 Documents/ChatRelayProject/src/chatRelay/DBManager.java
+++ Phase 3 Documents/ChatRelayProject/src/chatRelay/DBManager.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
-
 // PROBABLY CAN MAKE MORE THINGS PRIVATE
 
 public class DBManager {
@@ -41,12 +40,11 @@ public class DBManager {
 		for (AbstractUser user : users.values()) {
 			System.out.println(user);
 		}
-		
+
 		System.out.println("\n\nTest 'getUserById'");
 		AbstractUser u1 = getUserById("1");
 		System.out.println(u1);
-		
-		
+
 	}
 
 // TESTER - TO DELETE
@@ -103,23 +101,24 @@ public class DBManager {
 
 				String[] words = line.split("/");
 
-				String username = words[0];
-				String password = words[1];
-				String userId = words[2];
-				String firstName = words[3];
-				String lastName = words[4];
-				boolean isDisabled = words[5] == "true" ? true : false;
-				boolean isAdmin = words[6] == "true" ? true : false;
+				String id = words[0];
+				String ownerId = words[1];
+				String roomName = words[2];
+				String isPrivate = words[3];
+				String userIds = words[4];
 
-				AbstractUser newUser;
-
-				if (isAdmin) {
-					newUser = new ITAdmin(username, password, userId, firstName, lastName, isDisabled, isAdmin);
-				} else {
-					newUser = new User(username, password, userId, firstName, lastName, isDisabled, isAdmin);
-				}
-
-				users.put(userId, newUser);
+				
+				//create chat object, for each userid text get that from hashmap and then link that relationship . when you get a user you can also put their chat on their chat array
+				
+//				AbstractUser newUser;
+//
+//				if (isAdmin) {
+//					newUser = new ITAdmin(username, password, userId, firstName, lastName, isDisabled, isAdmin);
+//				} else {
+//					newUser = new User(username, password, userId, firstName, lastName, isDisabled, isAdmin);
+//				}
+//
+//				users.put(userId, newUser);
 
 			}
 			System.out.println("+++++++++++++++++++");
@@ -127,6 +126,7 @@ public class DBManager {
 			System.out.println("Error loading users: " + e.getMessage());
 		}
 	}
+
 	private void loadAllFiles() {
 //		loadUsers();
 //		loadChats();
@@ -137,9 +137,10 @@ public class DBManager {
 	public void sendUserAllFreshData() {
 	}
 
-	 public AbstractUser getUserById(String userId) {
-		 return users.get(userId);
-	 }
+	public AbstractUser getUserById(String userId) {
+		return users.get(userId);
+	}
+
 	// public Chat getChatById(String chatId) {}
 	// public Message getMessageById(String messageId) {}
 	// public List<User> fetchAllUsers() {}
