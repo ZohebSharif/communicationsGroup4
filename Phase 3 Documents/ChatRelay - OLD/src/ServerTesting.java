6bf7c635ce731ec7747d9f0235694ddff08cf24c
Talhah Shaik <tshaik3@horizon.csueastbay.edu>
2025-05-06 14:24:32 -0700

    Removed Old Code

--- Phase 3 Documents/ChatRelay - OLD/src/ServerTesting.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class ServerTesting {
-    public void testServerConnect() {
-    	Server server = new Server(1234, "nothing");
-    	server.connect();
-    }
-}
--- Phase 3 Documents/ChatRelay - OLD/src/User.java
+++
@@ -1,7 +0,0 @@
-public class User extends AbstractUser {
-
-    public User(String firstname, String lastname, String password) {
-        super(firstname, lastname, password);
-    }
-    
-}
